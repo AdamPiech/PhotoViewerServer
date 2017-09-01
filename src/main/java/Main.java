ed2e85d57a7ffd6932b4109395188204fc84504b
AdamPiech <adampiech7@op.pl>
2017-09-01 20:05:44 +0200

    S3 storage test

---
+++ src/main/java/Main.java
@@ -0,0 +1,78 @@
+
+import software.amazon.awssdk.async.AsyncRequestProvider;
+import software.amazon.awssdk.async.AsyncResponseHandler;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+import software.amazon.awssdk.utils.FunctionalUtils;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Created by Adam Piech on 2017-09-01.
+ */
+public class Main {
+
+    private static final String BUCKET = "photoviewerstore";
+    private static final String KEY = "photo";
+
+    public static void main(String[] args) {
+        putObject();
+        getObject();
+    }
+
+    private static void putObject() {
+        S3AsyncClient client = S3AsyncClient.create();
+
+        CompletableFuture<PutObjectResponse> future = client.putObject(
+                PutObjectRequest.builder()
+                        .bucket(BUCKET)
+                        .key(KEY)
+                        .build(),
+                AsyncRequestProvider.fromFile(Paths.get("~" + File.separator + "file.txt"))
+        );
+
+        future.whenComplete((resp, err) -> {
+            try {
+                if (resp != null) {
+                    System.out.println(resp);
+                } else {
+                    err.printStackTrace();
+                }
+            } finally {
+                FunctionalUtils.invokeSafely(client::close);
+            }
+        });
+    }
+
+    private static void getObject() {
+
+        S3AsyncClient client = S3AsyncClient.create();
+
+        final CompletableFuture<Void> future = client.getObject(
+                GetObjectRequest.builder()
+                        .bucket(BUCKET)
+                        .key(KEY)
+                        .build(),
+                AsyncResponseHandler.toFile(Paths.get("~" + File.separator + "file2")));
+
+        future.whenComplete((resp, err) -> {
+            try {
+                if (resp != null) {
+                    System.out.println(resp);
+                } else {
+                    err.printStackTrace();
+                }
+            } finally {
+                FunctionalUtils.invokeSafely(client::close);
+            }
+        });
+
+    }
+
+
+
+}
